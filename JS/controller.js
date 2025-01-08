f9a35f076fc28bec91ad8e3db37b279c7a042266
hoshen katz <hoshen.katz@gmail.com>
2025-01-08 11:30:31 +0200

    Meme editor starter

--- JS/controller.js
+++ JS/controller.js
@@ -1,5 +1,34 @@
 'use strict'
+let gElCanvas
+let gCtx 
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    onResize()
+}
 
 function toggleMenu() {
     document.body.classList.toggle('menu-open')
+}
+
+
+// main gallery //
+
+function onMemePicked(elImage) {
+    console.log('picked')
+    console.dir(elImage)
+    document.querySelector('.gallery-container').classList.add('hidden')
+    document.querySelector('.meme-editor').classList.remove('hidden')
+    drawImg(elImage.id)
+}
+
+function drawImg(id) {
+    console.log(id)
+    const elImg = new Image()
+    elImg.src = `img/${id}.jpg`
+
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
+    }
 }
---
+++ JS/service.js
@@ -0,0 +1,2 @@
+var gImgs = []
+var gMeme = {}
